b843f3243a4450e7ae2764c567c635123669e6a1
root <root@dliuzzo>
2024-11-02 21:44:55 +0000

    Advances

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Start the MariaDB service
+echo "DOCKERFILE SET UP SCRIPT STARTING "
+chown -R mysql:mysql /var/lib/mysql
 service mariadb start
-
+echo "TEST"
 # Wait for the server to start
 sleep 5 
 
@@ -21,33 +23,3 @@ fi
 
 # Start the MariaDB server in the foreground
 exec mysqld_safe
-
-
-
-
-# #!/bin/bash
-
-
-# if [ -f /var/lib/mysql/$DB_NAME ]; then
-#     echo "Database $DB_NAME already exists."
-# else
-
-#     sleep 5 
-
-#     echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mariadb -u root 
-
-#     echo "CREATE USER IF NOT EXISTS $DB_USERNAME@'localhost' IDENTIFIED BY '$DB_USERPASS';" | mariadb -u root  
-
-#     echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USERNAME@'%' IDENTIFIED BY '$DB_USERPASS';" | mariadb -u root 
-
-#     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';" | mariadb -u root 
-
-#     echo "FLUSH PRIVILEGE;" | mariadb -u root -p$DB_ROOTPASS 
-
-#     mysqladmin -uroot -p$DB_ROOTPASS shutdown --force
-
-#     sleep 5
-# fi 
-
-# exec mysqld_safe
-# # exec "$@"
