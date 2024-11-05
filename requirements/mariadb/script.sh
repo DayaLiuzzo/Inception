2fcd2f959394bc0adef1d333eb9dbee68eeff1e7
root <root@dliuzzo>
2024-11-05 14:58:56 +0000

    lol

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 /etc/init.d/mariadb start
 
 mysql_secure_installation << EOF
@@ -20,12 +21,11 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 echo "CREATING USER"
 mysql -e "CREATE USER IF NOT EXISTS \`${DB_USERNAME}\`@'localhost' IDENTIFIED BY '${DB_USERPASS}';"
 echo "GRANTING PRIVILEGES"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USERNAME}\`@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
-echo "ALTERING USER"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USERPASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USERNAME}\`@'%' IDENTIFIED BY '${DB_USERPASS}';"
 echo "FLUSHING"
 mysql -e "FLUSH PRIVILEGES;"
+# echo "ALTERING USER"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USERPASS}';"
 
 echo "CMDS DONE"
 /etc/init.d/mariadb stop
