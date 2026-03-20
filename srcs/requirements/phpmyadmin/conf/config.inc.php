a581d69c2997587e80c62fde02094d8700188c53
Daya Liuzzo <daya.liuzzo@gmail.com>
2026-03-20 20:23:40 +0100

    rm php custom image

--- srcs/requirements/phpmyadmin/conf/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$cfg['Servers'][1]['host'] = 'mariadb';
-$cfg['Servers'][1]['port'] = '3306';
-$cfg['Servers'][1]['auth_type'] = 'cookie';
-$cfg['blowfish_secret'] =  getenv('PMA_BLOWFISH_SECRET');
